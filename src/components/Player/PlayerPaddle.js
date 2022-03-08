04b00bb4ec7ce349e708d7d7c956b0830df87788
= <=>
2022-03-07 17:43:45 -0700

    Implemented Redux Toolkit

--- src/components/Player/PlayerPaddle.js
+++ src/components/Player/PlayerPaddle.js
@@ -5,12 +5,19 @@ const PlayerPaddle = (props) => {
   const player = props.player;
   const rotation = props.game.players.length === 2 && player.id == 1 ? "rotate-180" : "";
   const classNames = `paddle ${rotation}`;
+  let marble;
 
   return (
     <div className={classNames}>
       {constants.PADDLE_ITEMS.map((item, index) => {
         return (
-          <PlayerPaddleItem item={item} key={index} paddleBoardPlayer={player} game={props.game} />
+          <PlayerPaddleItem
+            item={item}
+            key={index}
+            paddleBoardPlayer={player}
+            game={props.game}
+            marble={marble}
+          />
         );
       })}
     </div>
